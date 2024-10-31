b40aeffc1c77be931eddd683c164267952a6bdff
Mathew <u116_matand_waw@technischools.com>
2024-10-31 14:13:58 +0100

    Basic GTK4 Rust window creation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use gtk4 as gtk;
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow};
+
+fn main() -> glib::ExitCode {
+    const APPID: &str = "com.matixandr.win.clipboardeditor";
+    let app = Application::builder().application_id(APPID).build();
+    app.connect_activate(build_ui);
+    app.run()
+}
+
+fn build_ui(app: &Application) {
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Windows Clipboard Editor")
+        .build();
+
+    window.present();
 }
